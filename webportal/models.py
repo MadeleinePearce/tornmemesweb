1e0f8c4c3549e5ff2a2d62ecd5158432180f9c52
Apkash <primeashwini992001@gmail.com>
2021-07-23 20:23:12 +0530

    Added Meme and Reaction Model

--- webportal/models.py
+++ webportal/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meme(models.Model):
+    username = models.CharField(max_length=40)
+    caption = models.CharField(max_length=80)
+    torn_id = models.PositiveBigIntegerField()
+    image_link = models.URLField()
+
+
+class Reaction(models.Model):
+    torn_id = models.PositiveBigIntegerField()
+
+    reactions =(
+        ('U', 'Upvote'),
+        ('D', 'Downvote'),
+        ) 
+
+    meme = models.ForeignKey(to = Meme, on_delete=models.CASCADE)
+    reaction = models.CharField(max_length=1, choices=reactions)
+
+
+
+    
