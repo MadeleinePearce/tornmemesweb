7489fac4855cbb32d3935bbe71e66cc7c4f00501
Priyam Harsh <priyamharsh.inno@gmail.com>
2021-07-29 18:51:29 +0530

    Fixed settings.py

--- tornmemesweb/settings.py
+++ tornmemesweb/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wsp7$amky9^m*8v7-efu#&ur%iick_snn8vy*vk*8k95s$=#1v"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "tornmemes.pythonanywhere.com"]
 
